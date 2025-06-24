41da97989236cbd1b50970503f777381f259086d
Josh Mckibbin <joshm@temple.edu>
2025-06-24 17:45:26 -0400

    Fix: remove unnecessary window.onload wrapper for read-only input fields on profile page

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-window.onload = function() {
-    if ( document.getElementById("profile-page") ) {
-        document.getElementById("first_name").readOnly = true;
-        document.getElementById("last_name").readOnly = true;
-        document.getElementById("nickname").readOnly = true;
-        document.getElementById("email").readOnly = true;
-    }
+if ( document.getElementById("profile-page") ) {
+    document.getElementById("first_name").readOnly = true;
+    document.getElementById("last_name").readOnly = true;
+    document.getElementById("nickname").readOnly = true;
+    document.getElementById("email").readOnly = true;
 }
